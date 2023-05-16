a86df823c2af6564780c1c0140ee71dba1e8b576
anhquangmobile <quangduong@tel4vn.com>
2023-05-16 14:53:36 +0700

    feat: first commit

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,12 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-pitel-voip';
+import { StyleSheet, View } from 'react-native';
+import { Counter } from 'react-native-pitel-voip';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
-
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
-
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <Counter />
     </View>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,6 @@
-export function multiply(a: number, b: number): Promise<number> {
-  return Promise.resolve(a * b);
-}
+// export function multiply(a: number, b: number): Promise<number> {
+//   return Promise.resolve(a * b);
+// }
+import { Counter } from './components/counter';
+
+export { Counter };
