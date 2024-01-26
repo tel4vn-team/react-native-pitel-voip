5b96597530811b843bf067f05f144957d1d41ed2
anhquangmobile <quangduong@tel4vn.com>
2024-01-26 18:16:34 +0700

    feat: 1.0.3

--- src/api/login_device.ts
+++ src/api/login_device.ts
@@ -14,6 +14,7 @@ export const getIceServer = async (): Promise<any> => {
     return data;
   } catch (error) {
     console.log(error);
+    return null;
   }
 };
 
--- src/components/pitel_sdk.jsx
+++ src/components/pitel_sdk.jsx
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { Platform, LogBox } from 'react-native';
 import { getFcmToken, NotificationListener } from '../notification/push_notif';
-import { registerDeviceToken, removeDeviceToken } from '../api/login_device';
+import {
+  registerDeviceToken,
+  removeDeviceToken,
+  getIceServer,
+} from '../api/login_device';
 
 export const PitelSDK = ({
   children,
@@ -32,6 +36,16 @@ export const PitelSDK = ({
     const deviceToken = Platform.OS == 'android' ? fcmToken : iosPushToken;
     setFcmToken(fcmToken);
     setDeviceToken(deviceToken);
+
+    const turn = await getIceServer();
+    let iceServers = [
+      { url: 'stun:stun.tel4vn.com:3478' },
+      { url: 'stun:stun.l.google.com:19302' },
+    ];
+    if (turn != null) {
+      iceServers.push(turn.data);
+    }
+
     const sdkOptionsInitialize = {
       sipOnly: true,
       sipDomain: `${sdkOptionsInit.sipDomain}:${sdkOptionsInit.port}`,
@@ -41,8 +55,11 @@ export const PitelSDK = ({
       debug: true,
       contactName: sdkOptionsInit.extension,
       viaHost: sdkOptionsInit.sipDomain,
+      preloadedRouteSet: [
+        `Route: <sip:${sdkOptionsInit.sipDomain}:${sdkOptionsInit.port};lr;sipml5-outbound;transport=udp>`,
+      ],
       contactParams: {
-        'transport': 'ws',
+        'transport': 'wss',
         'pn-provider': Platform.OS == 'android' ? 'fcm' : 'apns',
         'pn-prid': deviceToken,
         'pn-param':
@@ -51,6 +68,7 @@ export const PitelSDK = ({
             : `${sdkOptionsInit.teamId}.${sdkOptionsInit.bundleId}.voip`,
         'fcm-token': fcmToken,
       },
+      iceServers: iceServers,
     };
     setSdkOptions(sdkOptionsInitialize);
   };
